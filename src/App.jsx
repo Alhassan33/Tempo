dc13eb59ca93be97b2efe26b010b92e5f4589881
Mash.dev <103385466+Alhassan33@users.noreply.github.com>
2026-04-06 12:50:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Layout from "./components/Layout.jsx";
 import Market from "./pages/Market.jsx";
 import CollectionPage from "./pages/CollectionPage.jsx";
 import LaunchpadPage from "./pages/LaunchpadPage.jsx";
-import MintPage from "./pages/MintPage.jsx"; // New Detail Page
+import MintPage from "./pages/MintPage.jsx"; 
 import PortfolioPage from "./pages/PortfolioPage.jsx";
-import History from "./pages/History.jsx";
 import ManagePage from "./pages/ManagePage.jsx";
 import AdminPage from "./pages/AdminPage.jsx";
 import { LaunchpadProvider } from "./context/LaunchpadContext.jsx";
@@ -18,17 +17,15 @@ export default function App() {
         <PortfolioProvider>
           <Layout>
             <Routes>
+              {/* Secondary Market */}
               <Route path="/" element={<Market />} />
-              
-              {/* Marketplace Routes */}
               <Route path="/collection/:id" element={<CollectionPage />} />
-              <Route path="/history" element={<History />} />
               
-              {/* Launchpad Routes */}
+              {/* Primary Market (Launchpad) */}
               <Route path="/launchpad" element={<LaunchpadPage />} />
-              <Route path="/launchpad/:slug" element={<MintPage />} /> {/* Dynamic Mint Route */}
+              <Route path="/launchpad/:slug" element={<MintPage />} />
               
-              {/* User & Admin Routes */}
+              {/* User & Infrastructure */}
               <Route path="/portfolio" element={<PortfolioPage />} />
               <Route path="/manage" element={<ManagePage />} />
               <Route path="/admin" element={<AdminPage />} />
