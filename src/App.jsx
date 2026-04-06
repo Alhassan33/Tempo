f72f275fd6af10aecef2a001735b4e57737b0a22
Mash.dev <103385466+Alhassan33@users.noreply.github.com>
2026-04-06 12:44:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/Layout.jsx";
 import Market from "./pages/Market.jsx";
 import CollectionPage from "./pages/CollectionPage.jsx";
 import LaunchpadPage from "./pages/LaunchpadPage.jsx";
+import MintPage from "./pages/MintPage.jsx"; // New Detail Page
 import PortfolioPage from "./pages/PortfolioPage.jsx";
 import History from "./pages/History.jsx";
 import ManagePage from "./pages/ManagePage.jsx";
@@ -18,10 +19,17 @@ export default function App() {
           <Layout>
             <Routes>
               <Route path="/" element={<Market />} />
+              
+              {/* Marketplace Routes */}
               <Route path="/collection/:id" element={<CollectionPage />} />
-              <Route path="/launchpad" element={<LaunchpadPage />} />
-              <Route path="/portfolio" element={<PortfolioPage />} />
               <Route path="/history" element={<History />} />
+              
+              {/* Launchpad Routes */}
+              <Route path="/launchpad" element={<LaunchpadPage />} />
+              <Route path="/launchpad/:slug" element={<MintPage />} /> {/* Dynamic Mint Route */}
+              
+              {/* User & Admin Routes */}
+              <Route path="/portfolio" element={<PortfolioPage />} />
               <Route path="/manage" element={<ManagePage />} />
               <Route path="/admin" element={<AdminPage />} />
             </Routes>
